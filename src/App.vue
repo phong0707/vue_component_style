23edbbd5ec1c620c27d2c3b32644a3ed6f3885be
phong0707 <phongnhempet@gmail.com>
2025-08-26 16:43:22 +0700

    From_window

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header/>
+  <HeroSections />
+  <productsList/>
+  <FooterSection/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/header.vue';
+import HeroSections from '@/components/heroSections.vue';
+import productsList from '@/components/productsList.vue';
+import FooterSection from './components/footerSection.vue';
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Header,
+    HeroSections,
+    productsList,
+    FooterSection
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/productsList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="grid gap-x-8 grid-cols-3" >
+    <ProductCard v-for="(product,index) in products" :key="index" :name="product.name" :des="product.des" :price="product.price"/>
+
+  </div>
+
+  
+</template>
+<script>
+import ProductCard from './cards/productCard.vue';
+
+export default {
+    components:{
+        ProductCard
+    },
+  data() {
+    return {
+      products: [
+        {
+          name: "Wireless Headphones",
+          des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          price: 49.99,
+        },
+        {
+          name: "Smartphone",
+          des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          price: 699.99,
+        },
+        {
+          name: "Laptop",
+          des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          price: 999.99,
+        },
+        {
+          name: "Wireless Headphones",
+          des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          price: 49.99,
+        },
+        {
+          name: "Wireless Headphones",
+          des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          price: 49.99,
+        },
+        {
+          name: "Wireless Headphones",
+          des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          price: 49.99,
+        },
+      ],
+    };
+  },
+};
+</script>
